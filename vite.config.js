03ca520fe26b791304fe43afe007907442c912c2
Aaron Bushnell <aaron.bushnell@gmail.com>
2025-08-07 09:20:36 -0400

    Update Tailwind to 4.x

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import laravel from 'laravel-vite-plugin'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
 	plugins: [
@@ -12,6 +13,7 @@ export default defineConfig({
 			],
 			publicDirectory: 'resources/dist',
 		}),
+		tailwindcss(),
 		react(),
 	],
 	build: {
